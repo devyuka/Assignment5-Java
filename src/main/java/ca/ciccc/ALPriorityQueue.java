19e7723e1672657aa8c6a309e211edc9d34bf6ec
Yuka <yuka.0531.a@gmail.com>
2019-11-21 01:57:34 -0800

    update some methods in ALPriorityQueue

--- src/main/java/ca/ciccc/ALPriorityQueue.java
+++ src/main/java/ca/ciccc/ALPriorityQueue.java
@@ -2,7 +2,8 @@ package ca.ciccc;
 
 import java.util.ArrayList;
 
-public class ALPriorityQueue < E > implements VCPriorityQueue {
+public class ALPriorityQueue <K extends Comparable, V> implements VCPriorityQueue {
+
     private ArrayList < Entry > elementData;
     private int size;
 
@@ -21,16 +22,27 @@ public class ALPriorityQueue < E > implements VCPriorityQueue {
     }
 
     @Override
-    public Entry enqueue(Comparable key, Object value) throws IllegalArgumentException {
+    public Entry<K,V> enqueue(Comparable key, Object value) throws IllegalArgumentException {
         Entry newEntry = new Entry(key, value);
         elementData.add(newEntry);
         size++;
-        return new Entry(key, value);
+        return newEntry;
     }
 
     @Override
-    public Entry peek() {
-        return (size == 0) ? null : elementData.get(0);
+    public Entry<K, V> peek() {
+        if (size == 0) return null;
+
+        Entry<K, V> entry = elementData.get(0);
+        for(int i = 0; i < size; i++){
+            //Compare a current key with the previous key.
+            //If the returned value is smaller than 0, it means a current key is smaller than the previous key.
+            //You can get Entry with the smallest key. (smallest key is the highest value)
+            if(elementData.get(i).key.compareTo(entry.key) < 0){
+                entry = elementData.get(i);
+            }
+        }
+        return entry;
     }
 
     @Override
@@ -38,15 +50,7 @@ public class ALPriorityQueue < E > implements VCPriorityQueue {
         if (size == 0) {
             return null;
         } else {
-            int smallestKey = (int) elementData.get(0).getKey();
-            Entry smallestKeyEntry = elementData.get(0);
-
-            for (int i = 0; i < size(); i++) {
-                if (smallestKey > (int) elementData.get(i).getKey()) {
-                    smallestKey = (int) elementData.get(i).getKey();
-                    smallestKeyEntry = elementData.get(i);
-                }
-            }
+            Entry smallestKeyEntry = peek();
 
             elementData.remove(smallestKeyEntry);
             size--;
@@ -54,21 +58,14 @@ public class ALPriorityQueue < E > implements VCPriorityQueue {
         }
     }
 
-    public Entry poll() {
-        if (size == 0) {
-            return null;
+    @Override
+    public VCPriorityQueue<K, V>  merge(VCPriorityQueue other) {
+
+        while(other.peek() != null){
+            Entry<K, V> entry = other.dequeueMin();
+            enqueue(entry.getKey(),entry.getValue());
         }
 
-        Entry firstElement = elementData.get(0);
-        elementData.remove(firstElement);
-        size--;
-
-        return firstElement;
-    }
-
-    @Override
-    public VCPriorityQueue merge(VCPriorityQueue other) {
-        elementData.addAll(((ALPriorityQueue) other).elementData);
         size += other.size();
         return this;
     }
